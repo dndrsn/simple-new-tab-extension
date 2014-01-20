d08eb15c4183a703d30168586513f187c2c42f97
Dan Anderson <daniel.anderson@jouleww.com>
2014-01-20 15:31:31 -0500

    original implementation

---
+++ background.js
@@ -0,0 +1,12 @@
+
+/*globals chrome */
+
+(function() {
+
+	"use strict";
+
+	window.getBookmarks = function() {
+		return chrome.bookmarks;
+	};
+
+})();
---
+++ new-tab.js
@@ -0,0 +1,31 @@
+
+/*globals chrome */
+
+(function() {
+
+	var bgPage = chrome.extension.getBackgroundPage();
+	var bookmarks = bgPage.getBookmarks();
+	var ul = document.querySelector('ul.bookmarks');
+
+
+	function printBookmarks(bookmarks) {
+
+		var html = '';
+
+	  bookmarks.forEach(function(bookmark) {
+			html += "<li><a href='" + bookmark.url + "'><img class='favicon' src='chrome://favicon/" + bookmark.url + "' /> " + bookmark.title + "</a></li>";
+	  });
+
+	  ul.innerHTML = html;
+
+	}
+
+
+	bookmarks.getChildren('1', function(bookmarks) {
+	  printBookmarks(bookmarks);
+	});
+
+})();
+
+
+
