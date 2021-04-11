77d0c6b12e93296b9fc25ae1d29cc10047291981
Dan Anderson <dan@vshift.com>
2021-04-11 17:31:43 -0400

    working build

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,17 @@
 const esbuild = require('esbuild');
+const yaml = require('js-yaml');
+const fse = require('fs-extra');
+const cpx = require('cpx');
+const path = require('path');
+const { each } = require('lodash');
 
 const esbuildConfig = require('../esbuild.config');
 
 
-const build = async () => {
+const copyConfig = yaml.load(fse.readFileSync('copy.config.yml'));
+
+
+const compile = async () => {
   try {
     await esbuild.build(esbuildConfig);
   }
@@ -13,5 +21,15 @@ const build = async () => {
 };
 
 
-build();
+const copy = async () => {
+  each(copyConfig.sources, source => {
+    const sourcePath = typeof source === 'string' ? source : source.path;
+    const destPath = source.context ? path.join(copyConfig.dest, source.context) : copyConfig.dest;
+    cpx.copy(sourcePath, destPath);
+  });
+};
+
+
+compile();
+copy();
 
